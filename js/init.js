3d8de7976910e5b05e60961fddd5c6984365bd13
Ayush Kumar Singh <57259393+aksse99@users.noreply.github.com>
2021-10-21 17:16:20 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); 
+})(jQuery);
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+$(document).ready(function(){
+    $('select').formSelect();
+});
+$(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
